2beef9ac97e00611abe4650d5751b1fd1b2f3a7d
Jose Eduardo Nunes Lino <eduardo.lino@pucpr.br>
2024-03-20 21:15:12 -0300

    pagina de cadastro

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,19 @@
+import com.sun.security.jgss.GSSUtil;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
 
 
+        System.out.println("Pagina de cadastro");
+        System.out.println("Usuario: AAAAA");
+        System.out.println("Senha: ****");
+        System.out.println("CPF: 999999999");
+        System.out.println("E-mail: eduardo.lino@pucpr.br");
+        System.out.println("(Cadastrar)");
 
-        }
+
+
     }
 }
